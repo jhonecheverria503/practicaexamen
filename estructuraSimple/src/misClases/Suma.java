a41b04a2d45cb1cabbb527e8e4d58cebc06c0397
JHON <JHON@JHON-PC>
2018-04-14 13:46:12 -0600

    Primer numero

---
+++ estructuraSimple/src/misClases/Suma.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package misClases;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author JHON
+ */
+public class Suma {
+   
+    public static void main(String[] args) {
+        
+        double n1, n2,r;
+        
+        Scanner numero = new Scanner(System.in);
+        
+        System.out.println("Ingrese un numero");
+        n1= numero.nextDouble();
+        
+        
+        
+    }
+    
+    
+    
+}
